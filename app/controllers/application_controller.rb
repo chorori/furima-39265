c888865d518e52e00cc8a30764089a394a641d40
chorori <abc123theend@gmail.com>
2023-08-18 11:21:28 +0900

    ルートパスへアクセスを設定した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  # Basic認証
+  before_action :basic_auth
+  # Basic認証
+
+
+  private
+
+  # Basic認証
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
+  end
+  # Basic認証
 end
