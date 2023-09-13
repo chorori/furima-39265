040b1de78c4c76a961254a834a0b40377b61f80d
chorori <abc123theend@gmail.com>
2023-09-14 00:31:30 +0900

    アクティブハッシュとカテゴリーの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,17 @@ class Item < ApplicationRecord
   validates :description, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipping_fee_id, presence: true
-  validates :shipping_region_id, presence: true
-  validates :shipping_day_id, presence: true
+  validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_region_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   has_one_attached :image
   validates :image, presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :shipping_region
+  belongs_to :shipping_day
 end
