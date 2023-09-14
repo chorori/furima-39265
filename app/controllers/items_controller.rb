af74ee092e652fa157584fdf1e3d9a0971b778cf
chorori <abc123theend@gmail.com>
2023-09-14 15:49:09 +0900

    エラーハンドリングによる修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,5 +4,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = current_user.items.build(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_fee_id, :shipping_region_id, :shipping_day_id, :price)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'items/index'
   root to: "items#index"
-  get 'items/new', to: 'items#new', as: 'new_item'
+  resources :items, only: [:index, :new, :create]
 end
