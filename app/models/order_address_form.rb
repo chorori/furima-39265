f6a81cf9d794a41cf04ca4057889143c6eecac46
chorori <abc123theend@gmail.com>
2023-10-02 13:18:19 +0900

    バリデーションの修正

--- app/models/order_address_form.rb
+++ app/models/order_address_form.rb
@@ -1,17 +1,15 @@
 class OrderAddressForm
   include ActiveModel::Model
   attr_accessor :postal_code, :shipping_region_id, :city, :street_address, :building_name, :phone_number, :user_id, :item_id, :token
-  
-  with_options presence: true do
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
-    validates :shipping_region_id, numericality: { other_than: 0, message: "can't be blank" }
-    validates :city
-    validates :street_address
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid." }
-    validates :user_id
-    validates :item_id
-    validates :token
-  end
+
+  validates :token, presence: { message: "can't be blank" }
+  validates :postal_code, presence: { message: "can't be blank" }, format: { with: /\A\d{3}-\d{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
+  validates :shipping_region_id, presence: { message: "Prefecture can't be blank" }, numericality: { other_than: 0, message: "must be selected" }
+  validates :city, presence: { message: "can't be blank" }
+  validates :street_address, presence: { message: "can't be blank" }
+  validates :phone_number, presence: { message: "can't be blank" }, format: { with: /\A\d{10,11}\z/, message: "is invalid. Input only number" }, length: { in: 10..11, message: "is too short" }
+  validates :user_id, presence: { message: "can't be blank" }
+  validates :item_id, presence: { message: "can't be blank" }
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
